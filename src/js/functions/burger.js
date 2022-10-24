11c084a3f3bacd3f362b38d8b9bcfda2d2d4fa16
magical01 <denis.verzakov.015@gmail.com>
2022-10-24 19:11:38 +0500

    feat: add adaptive

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -6,10 +6,14 @@ import { enableScroll } from '../functions/enable-scroll';
   const menu = document?.querySelector('[data-menu]');
   const menuItems = document?.querySelectorAll('[data-menu-item]');
   const overlay = document?.querySelector('[data-menu-overlay]');
+  const logo = document?.querySelector('.header__logo');
+  const blur = document?.querySelector('.overlay');
 
   burger?.addEventListener('click', (e) => {
     burger?.classList.toggle('burger--active');
     menu?.classList.toggle('menu--active');
+    blur?.classList.toggle('overlay--active');
+    logo?.classList.toggle('header__logo--hide')
 
     if (menu?.classList.contains('menu--active')) {
       burger?.setAttribute('aria-expanded', 'true');
@@ -22,11 +26,13 @@ import { enableScroll } from '../functions/enable-scroll';
     }
   });
 
-  overlay?.addEventListener('click', () => {
+  blur?.addEventListener('click', () => {
     burger?.setAttribute('aria-expanded', 'false');
     burger?.setAttribute('aria-label', 'Открыть меню');
     burger.classList.remove('burger--active');
     menu.classList.remove('menu--active');
+    blur?.classList.remove('overlay--active');
+    logo?.classList.remove('header__logo--hide')
     enableScroll();
   });
 
@@ -36,6 +42,8 @@ import { enableScroll } from '../functions/enable-scroll';
       burger?.setAttribute('aria-label', 'Открыть меню');
       burger.classList.remove('burger--active');
       menu.classList.remove('menu--active');
+      blur?.classList.remove('overlay--active');
+      logo?.classList.remove('header__logo--hide')
       enableScroll();
     });
   });
